eef6f7aefadb8697bc8a892d499df1ed3e10778b
S. M. Mohiuddin Khan Shiam <147746955+mohiuddin-khan-shiam@users.noreply.github.com>
2025-02-04 02:40:20 +0600

    Create hello_world.js

---
+++ Basics/hello_world.js
@@ -0,0 +1,11 @@
+// Basics/hello_world.js
+
+// This program prints "Hello, World!" to the console
+
+// Defining a function to print the message
+function greet() {
+    console.log("Hello, World!");
+}
+
+// Calling the function
+greet();
